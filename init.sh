0a052fcef530e6342e0a4b2b78094233b3e0aa98
Hunter Hough <hunter.hough@gmail.com>
2018-04-21 14:45:54 -0400

    removed storj

--- init.sh
+++ init.sh
@@ -7,19 +7,12 @@ sudo apt-get install -y git
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install ethereum
-
-#C and golang compilers
+sudo apt-get install -y ethereum
+sudo apt-get install -y curl
 sudo apt-get install -y build-essential golang
+
+# ethereum
 mkdir go && cd go
 mkdir -p src/github.com/ethereum pkg bin && cd src/github.com/ethereum
 git clone https://github.com/ethereum/go-ethereum
 cd go-ethereum && make geth
-
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y npm
-
-mkdir ~/storj
-cd ~/storj
-npm install storj-lib --save
